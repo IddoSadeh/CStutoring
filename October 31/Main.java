e7d6366815d648a0681e58f9704dbf9e00b5b696
IddoSadeh <44779994+IddoSadeh@users.noreply.github.com>
2021-11-07 16:16:25 -0800

    code and notes from october 31
    will go over later

---
+++ October 31/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+       String a = new String("hello");
+       for(int i=0;i<10;i++)
+       {
+           
+           double c = Math.random();
+           System.out.println(c);
+       } 
+    }
+    
+}
